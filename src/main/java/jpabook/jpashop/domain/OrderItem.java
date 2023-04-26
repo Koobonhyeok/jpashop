1bbf1d1739c4f437a7b03e3ff1162c0a8cca6f78
koobonhyeok <korea6441@gmail.com>
2023-04-26 23:04:19 +0900

    repository생성 및 Service 작성

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -24,4 +25,25 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    // 다른곳에서 사용하지 않게 하기위해
+    protected OrderItem(){}
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem( item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    public void cancel(){
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice(){
+        return getOrderPrice() * getCount();
+    }
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,5 +18,23 @@ public abstract class Item {
     private String name;
     private int price;
     private  int stockQuantity;
+
+    /**
+     * stock 증가
+     */
+    public void addStock( int quantity ){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock( int quantity ){
+        int restStock = this.stockQuantity - quantity;
+        if( restStock < 0 ){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
