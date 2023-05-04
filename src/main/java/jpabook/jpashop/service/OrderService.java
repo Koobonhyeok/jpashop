8301d72fc6210f482bea398e76eb835dbf363bac
koobonhyeok <korea6441@gmail.com>
2023-05-04 11:23:35 +0900

    검색 및 주문

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -62,71 +62,7 @@ public class OrderService {
         order.cancel();
     }
 
-    private final EntityManager em;
-    /**
-     * 검색
-     */
-
-    public List<Order> findAll( OrderSearch orderSearch ){
-        String jpql = "select o from Order o join o.member m";
-        boolean condition = true;
-
-        if( orderSearch.getOrderStatus() != null ){
-            if( condition ){
-                jpql += " where";
-                condition = false;
-            }else{
-                jpql += " and";
-            }
-            jpql += "o.status = :status";
-        }
-
-        if( StringUtils.hasText( orderSearch.getMemberName() ) ){
-            if( condition ){
-                jpql += " where";
-                condition = false;
-            }else{
-                jpql += " and";
-            }
-            jpql += " m.name like :name ";
-        }
-
-        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000);
-
-        if( orderSearch.getOrderStatus() != null ){
-            query = query.setParameter("status", orderSearch.getOrderStatus());
-        }
-        if( StringUtils.hasText(orderSearch.getMemberName()) ){
-            query = query.setParameter("name", orderSearch.getMemberName());
-        }
-
-        List<Order> resultList = query.getResultList();
-        return resultList;
-    }
-
-    /**
-     * JPA Criteria
-     */
-    public List<Order> findAllByCriteria(OrderSearch orderSearch){
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
-        Root<Order> o = cq.from(Order.class);
-        Join<Object, Object> m = o.join("member", JoinType.INNER);
-
-        List<Predicate> criteria = new ArrayList<>();
-
-        if( orderSearch.getOrderStatus() != null ){
-            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
-            criteria.add(status);
-        }
-
-        if(StringUtils.hasText(orderSearch.getMemberName())){
-            Predicate name = cb.like( m.get("name"), "%"+orderSearch.getMemberName()+"%" );
-            criteria.add(name);
-        }
-
-        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
-        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
-        return query.getResultList();
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAllByCriteria(orderSearch);
     }
 }
